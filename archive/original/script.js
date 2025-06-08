95880cec48280ac3cd11d3794dbfbc324c7ed61e
ravewyvern <kabookeyhead@gmail.com>
2025-06-07 17:55:40 -0700

    started new version of website without AI

--- archive/original/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    const menu = document.getElementById('hamburger-menu');
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
-});
