929cc89e5d93c5ab5b5114b0ff6b3cf582851153
ravewyvern <nova@novawolf.me>
2025-10-20 21:52:21 -0700

    Erased a bunch of useless code and stuff and fixed a few things

--- README.md
+++
@@ -1,7 +0,0 @@
-# ravewyvern.github.io
-
-My website WIP
-
-Go to the website at https://ravewyvern.me
-
-Reminders and todo
--- tools/protected/87gyofd7ogasd9y70/fasiuhfdsaiuhfasd.js
+++
@@ -1,6 +0,0 @@
-console.log("Secret page JS loaded!");
-document.getElementById('logout').addEventListener('click', () => {
-    localStorage.removeItem('stay_key_hex');
-    // return to login page
-    window.location.href = '../index.html'; // or the login file path
-});
